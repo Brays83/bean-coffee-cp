06593255265a944d0300be85cc533a2e1f58391d
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2025-03-17 22:27:41 -0500

    Inicializacion del proyecto

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser';
+
+const config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+new Phaser.Game(config);
+
+function preload() {
+  this.load.image('player','src/assets/sprites/1.png')
+  this.load.image('scene')
+
+}
+function create() {
+  this.add.image(400, 300, 'player')
+
+}
+function update() {
+
+}
+
+
