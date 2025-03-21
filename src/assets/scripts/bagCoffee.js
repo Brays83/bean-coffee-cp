39d6e5666e28741114b12429144db5e19a19d945
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2025-03-21 02:33:26 -0500

    Creamos logica del saco

---
+++ src/assets/scripts/bagCoffee.js
@@ -0,0 +1,37 @@
+import Phaser from "phaser";
+
+export class BagCoffee extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene,x,y,texture){
+        super(scene,x,y,texture);
+
+        //Variables de la clase
+        this.scene = scene;
+        this.x = x;
+        this.y = y;
+        this.texture = texture;
+
+        this.scene.physics.add.existing(this);
+
+        this.scene.add.existing(this)
+
+        this.setImmovable(true);
+        if(!this.scene.anims.exists("bag-animations")){
+
+            this.scene.anims.create({
+                key: "bag-animations",
+                frames: [
+                    {key: "bag0"},
+                    {key: "bag1"},
+                    {key: "bag2"}
+                ],
+                frameRate: 5,
+                repeat: -1  
+            });
+
+        }
+
+        
+
+        this.play("bag-animations");
+    }
+}
