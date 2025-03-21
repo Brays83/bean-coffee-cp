48f7a5b241b563ec92cdf79320aef24979446294
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2025-03-21 02:36:01 -0500

    añádimos precarga de sprites y inputs de sacos

--- src/assets/scripts/mainScene.js
+++ src/assets/scripts/mainScene.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import { Player } from "./player.js";
 import { Mesa } from "./mesa.js";
+import { BagCoffee } from "./bagCoffee.js";
 
 export class MainScene extends Phaser.Scene{
     constructor(){
@@ -16,7 +17,32 @@ export class MainScene extends Phaser.Scene{
 
     preload(){
         this.load.image("scene","src/assets/sprites/scene/1.png");
-        this.load.image("player","src/assets/sprites/player/1.png");
+
+        this.load.image("bag0","src/assets/sprites/bag/1.png");
+        this.load.image("bag1","src/assets/sprites/bag/2.png");
+        this.load.image("bag2","src/assets/sprites/bag/3.png");
+
+        this.load.image("bag-table-0","src/assets/sprites/bag/497.png");
+        this.load.image("bag-table-1","src/assets/sprites/bag/496.png");
+        this.load.image("bag-table-2","src/assets/sprites/bag/511.png");
+  
+        
+        this.load.image("player-free","src/assets/sprites/player/1.png");
+        this.load.image("player-coffe1","src/assets/sprites/player/2.png");
+        this.load.image("player-coffe2","src/assets/sprites/player/3.png");
+        this.load.image("player-coffe3","src/assets/sprites/player/4.png");
+        this.load.image("player-coffe4","src/assets/sprites/player/5.png");
+        this.load.image("player-coffe5","src/assets/sprites/player/6.png");
+        this.load.image("player-coffe-tired","src/assets/sprites/player/7.png");
+        this.load.image("player-anvil","src/assets/sprites/player/8.png");
+        this.load.image("player-fish","src/assets/sprites/player/9.png");
+        this.load.image("player-vase","src/assets/sprites/player/10.png");
+
+        this.load.image("player-cry-0","src/assets/sprites/player/111.png");
+        this.load.image("player-cry-1","src/assets/sprites/player/112.png");
+        this.load.image("player-cry-2","src/assets/sprites/player/113.png");
+
+
         this.load.image("mesa","src/assets/sprites/mesa/1.png");
         this.load.image("nieve","src/assets/sprites/scene/342.png");
         this.load.image("nieve-piso","src/assets/sprites/scene/529.png");
@@ -24,6 +50,8 @@ export class MainScene extends Phaser.Scene{
 
     }
     create(){
+
+        
         
         //  Cargamos el escenario base
         this.add.image(0, 0, "scene").setOrigin(0, 0);
@@ -34,7 +62,7 @@ export class MainScene extends Phaser.Scene{
 
         //Cargamos el jugador
         //new Player(*Envio escena actual*, *Posicion x*, *Posicion y*, *Envio Textura*);
-        this.player = new Player(this, 400, 350, "player");
+        this.player = new Player(this, 400, 350, "player-free");
         
         
         //Cargamos la mesa
@@ -46,6 +74,30 @@ export class MainScene extends Phaser.Scene{
 
         //Cargamos el camion
         this.add.image(590, 70, "camion").setOrigin(0, 0);
+        
+
+        //Cargamos la bolsa de cafe
+        this.input.keyboard.on("keydown-" + "SPACE", (e) => {
+            this.bagCoffee = new BagCoffee(this, 600, 300, "bag0");
+            this.bagCoffee.setOrigin(0,0);
+            this.physics.add.collider(this.player, this.bagCoffee,this.player.handleCollisionWithBag,null,this.player);
+        });
+
+        this.input.keyboard.on("keydown-" + "R", () => {
+            this.scene.restart();
+        });
+        
+        
+
+        //Establece cosas fijas
+        
+
+        //Agregar fisicas
+        this.physics.add.collider(this.player, this.mesa,this.player.handleCollisionWithTable,null,this.player);
+        
+
+
+       
 
         /**
          * Crear variable local
@@ -57,7 +109,11 @@ export class MainScene extends Phaser.Scene{
 
     }
     update(){
+        
+        console.log(`vidas del pinguino: ${this.player.lives}`)
         this.player.update();
+        
+
         
     }
 }
